6b85cb57b42d03fe1efcb35f3517ef374979b58e
Anthony Scopatz <scopatz@gmail.com>
2014-03-25 19:25:49 -0700

    today's work

--- data_to_json.py
+++ data_to_json.py
@@ -7,12 +7,13 @@ import time
 
 import numpy as np
 
+from tools import diff_last
+
 def year_to_ms(y):
     """converts years to milliseconds."""
     x = datetime(y, 1, 1)
     x = time.mktime(x.timetuple()) * 1000
     return int(x)
-
 
 def main():
     parser = ArgumentParser()
@@ -21,10 +22,7 @@ def main():
     csv = np.recfromcsv(ns.filename, delimiter=',', filling_values=np.nan, 
                         case_sensitive=True, deletechars='', replace_space=' ')
     dates = map(year_to_ms, csv['year'])
-    subtot = np.array(csv[csv.dtype.names[1]])
-    for k in csv.dtype.names[2:-1]:
-        subtot += csv[k]
-    csv[csv.dtype.names[-1]] -= subtot
+    diff_last(csv)
     j = [{"key": k, "values": zip(dates, np.asarray(csv[k], 'f8'))} \
          for k in csv.dtype.names[1:]]
     with open(os.path.splitext(ns.filename)[0] + '.json', 'w') as f:
