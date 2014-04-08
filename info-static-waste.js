654f3922259bdc0d279d77420431041df8664b4d
Anthony Scopatz <scopatz@gmail.com>
2014-04-08 10:11:21 -0700

    fixed static waste

--- info-static-waste.js
+++ info-static-waste.js
@@ -16,16 +16,16 @@ var pack = d3.layout.pack()
 // Time step 1 
 //
 
-var year1 = d3.select("body").insert("svg:svg", "h2")
+var fc1 = d3.select("body").insert("svg:svg", "h2")
     .attr("width", w)
     .attr("height", h)
   .append("svg:g")
     .attr("transform", "translate(" + (w - r) / 2 + "," + (h - r) / 2 + ")");
 
-d3.json("year2000-waste.json", function(data) {
+d3.json("info-raw-data-run1-2000_2050_2100-waste.json", function(data) {
   node = root = data;
   var nodes = pack.nodes(root);
-  year1.selectAll("circle")
+  fc1.selectAll("circle")
       .data(nodes)
     .enter().append("svg:circle")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
@@ -33,7 +33,7 @@ d3.json("year2000-waste.json", function(data) {
       .attr("cy", function(d) { return d.y; })
       .attr("r", function(d) { return d.r; })
 
-  year1.selectAll("text")
+  fc1.selectAll("text")
       .data(nodes)
     .enter().append("svg:text")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
@@ -49,16 +49,16 @@ d3.json("year2000-waste.json", function(data) {
 // Time step 2 
 //
 
-var year2 = d3.select("body").insert("svg:svg", "h2")
+var fc2 = d3.select("body").insert("svg:svg", "h2")
     .attr("width", w)
     .attr("height", h)
   .append("svg:g")
     .attr("transform", "translate(" + (w - r) / 2 + "," + (h - r) / 2 + ")");
 
-d3.json("year2050-waste.json", function(data) {
+d3.json("info-raw-data-run3-2000_2050_2100-waste.json", function(data) {
   node = root = data;
   var nodes = pack.nodes(root);
-  year2.selectAll("circle")
+  fc2.selectAll("circle")
       .data(nodes)
     .enter().append("svg:circle")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
@@ -66,7 +66,7 @@ d3.json("year2050-waste.json", function(data) {
       .attr("cy", function(d) { return d.y; })
       .attr("r", function(d) { return d.r; })
 
-  year2.selectAll("text")
+  fc2.selectAll("text")
       .data(nodes)
     .enter().append("svg:text")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
@@ -83,16 +83,16 @@ d3.json("year2050-waste.json", function(data) {
 // Time step 3
 //
 
-var year3 = d3.select("body").insert("svg:svg", "h2")
+var fc3 = d3.select("body").insert("svg:svg", "h2")
     .attr("width", w)
     .attr("height", h)
   .append("svg:g")
     .attr("transform", "translate(" + (w - r) / 2 + "," + (h - r) / 2 + ")");
 
-d3.json("year2100-waste.json", function(data) {
+d3.json("info-raw-data-run5-2000_2050_2100-waste.json", function(data) {
   node = root = data;
   var nodes = pack.nodes(root);
-  year3.selectAll("circle")
+  fc3.selectAll("circle")
       .data(nodes)
     .enter().append("svg:circle")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
@@ -100,7 +100,7 @@ d3.json("year2100-waste.json", function(data) {
       .attr("cy", function(d) { return d.y; })
       .attr("r", function(d) { return d.r; })
 
-  year3.selectAll("text")
+  fc3.selectAll("text")
       .data(nodes)
     .enter().append("svg:text")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
