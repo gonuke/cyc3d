2a62bf32e96cf4477a24473e1b50dea71bbec630
Anthony Scopatz <scopatz@gmail.com>
2014-04-08 15:48:01 -0700

    updated dynamic cost info

--- info-dynamic-cost.js
+++ info-dynamic-cost.js
@@ -13,7 +13,7 @@ var pack = d3.layout.pack()
 
 
 // 
-// Time step 1 
+// Fuel Cycle 1 
 //
 
 var fc1 = d3.select("body").insert("svg:svg", "h2")
@@ -22,21 +22,26 @@ var fc1 = d3.select("body").insert("svg:svg", "h2")
   .append("svg:g")
     .attr("transform", "translate(" + (w - r) / 2 + "," + (h - r) / 2 + ")");
 
-d3.json("info-raw-data-run1-2000_2050_2100-cost.json", function(data) {
-  node = root = data;
-  var nodes = pack.nodes(root);
-  fc1.selectAll("circle")
+function load_fc1(fname) {
+  d3.json(fname, function(data) {
+    node = root = data;
+    var nodes = pack.nodes(root);
+    fc1.selectAll("circle")
+      .remove();
+    fc1.selectAll("circle")
       .data(nodes)
-    .enter().append("svg:circle")
+      .enter().append("svg:circle")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
       .attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; })
       .attr("r", function(d) { return d.r; })
       .on("click", function(d) { return zoom1(node == d ? root : d); });
 
-  fc1.selectAll("text")
+    fc1.selectAll("text")
+      .remove();
+    fc1.selectAll("text")
       .data(nodes)
-    .enter().append("svg:text")
+      .enter().append("svg:text")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
       .attr("x", function(d) { return d.x; })
       .attr("y", function(d) { return d.y; })
@@ -45,8 +50,9 @@ d3.json("info-raw-data-run1-2000_2050_2100-cost.json", function(data) {
       .style("opacity", function(d) { return d.r > 20 ? 1 : 0; })
       .text(function(d) { return d.name; });
 
-  d3.select(window).on("click", function() { zoom1(root); });
-});
+    d3.select(window).on("click", function() { zoom1(root); });
+  });
+}
 
 function zoom1(d, i) {
   var k = r / d.r / 2;
@@ -70,8 +76,10 @@ function zoom1(d, i) {
   d3.event.stopPropagation();
 }
 
+load_fc1("years/info-raw-data-run1-2000-cost.json");
+
 // 
-// Time step 2 
+// Fuel Cycle 2 
 //
 
 var fc2 = d3.select("body").insert("svg:svg", "h2")
@@ -80,21 +88,26 @@ var fc2 = d3.select("body").insert("svg:svg", "h2")
   .append("svg:g")
     .attr("transform", "translate(" + (w - r) / 2 + "," + (h - r) / 2 + ")");
 
-d3.json("info-raw-data-run3-2000_2050_2100-cost.json", function(data) {
-  node = root = data;
-  var nodes = pack.nodes(root);
-  fc2.selectAll("circle")
+function load_fc2(fname) {
+  d3.json(fname, function(data) {
+    node = root = data;
+    var nodes = pack.nodes(root);
+    fc2.selectAll("circle")
+      .remove();
+    fc2.selectAll("circle")
       .data(nodes)
-    .enter().append("svg:circle")
+      .enter().append("svg:circle")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
       .attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; })
       .attr("r", function(d) { return d.r; })
       .on("click", function(d) { return zoom2(node == d ? root : d); });
 
-  fc2.selectAll("text")
+    fc2.selectAll("text")
+      .remove();
+    fc2.selectAll("text")
       .data(nodes)
-    .enter().append("svg:text")
+      .enter().append("svg:text")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
       .attr("x", function(d) { return d.x; })
       .attr("y", function(d) { return d.y; })
@@ -103,8 +116,9 @@ d3.json("info-raw-data-run3-2000_2050_2100-cost.json", function(data) {
       .style("opacity", function(d) { return d.r > 20 ? 1 : 0; })
       .text(function(d) { return d.name; });
 
-  d3.select(window).on("click", function() { zoom2(root); });
-});
+    d3.select(window).on("click", function() { zoom2(root); });
+  });
+}
 
 function zoom2(d, i) {
   var k = r / d.r / 2;
@@ -128,6 +142,8 @@ function zoom2(d, i) {
   d3.event.stopPropagation();
 }
 
+load_fc2("years/info-raw-data-run3-2000-cost.json");
+
 // 
 // Time step 3
 //
@@ -138,21 +154,26 @@ var fc3 = d3.select("body").insert("svg:svg", "h2")
   .append("svg:g")
     .attr("transform", "translate(" + (w - r) / 2 + "," + (h - r) / 2 + ")");
 
-d3.json("info-raw-data-run5-2000_2050_2100-cost.json", function(data) {
-  node = root = data;
-  var nodes = pack.nodes(root);
-  fc3.selectAll("circle")
+function load_fc3(fname) {
+  d3.json(fname, function(data) {
+    node = root = data;
+    var nodes = pack.nodes(root);
+    fc3.selectAll("circle")
+      .remove();
+    fc3.selectAll("circle")
       .data(nodes)
-    .enter().append("svg:circle")
+      .enter().append("svg:circle")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
       .attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; })
       .attr("r", function(d) { return d.r; })
       .on("click", function(d) { return zoom3(node == d ? root : d); });
 
-  fc3.selectAll("text")
+    fc3.selectAll("text")
+      .remove();
+    fc3.selectAll("text")
       .data(nodes)
-    .enter().append("svg:text")
+      .enter().append("svg:text")
       .attr("class", function(d) { return d.children ? "parent" : "child"; })
       .attr("x", function(d) { return d.x; })
       .attr("y", function(d) { return d.y; })
@@ -161,8 +182,9 @@ d3.json("info-raw-data-run5-2000_2050_2100-cost.json", function(data) {
       .style("opacity", function(d) { return d.r > 20 ? 1 : 0; })
       .text(function(d) { return d.name; });
 
-  d3.select(window).on("click", function() { zoom3(root); });
-});
+    d3.select(window).on("click", function() { zoom3(root); });
+  });
+};
 
 function zoom3(d, i) {
   var k = r / d.r / 2;
@@ -184,4 +206,17 @@ function zoom3(d, i) {
 
   node = d;
   d3.event.stopPropagation();
+}
+
+load_fc3("years/info-raw-data-run5-2000-cost.json");
+
+function load_fcs(year) {
+  load_fc1("years/info-raw-data-run1-" + year + "-cost.json");
+  load_fc2("years/info-raw-data-run3-" + year + "-cost.json");
+  load_fc3("years/info-raw-data-run5-" + year + "-cost.json");
+}
+
+function yearUpdate(year) {
+  document.querySelector('#yearlabel').value = year;
+  load_fcs(year);
 }
