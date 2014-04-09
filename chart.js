0281fbbfd492614228a7f91711ae179dedc370c4
Paul P.H. Wilson <wilsonp@engr.wisc.edu>
2014-04-09 01:43:51 -0500

    Try to get variable chart size back.

--- chart.js
+++ chart.js
@@ -3,7 +3,7 @@ function chart(dynamic,yLabel,yMax,json_data1,json_data2,json_data3)
 {
     var chartAttrs = {
         "width": '100%',
-        "height": '400px'
+        "height": '40%'
     };
     
     var chartMargins = {
